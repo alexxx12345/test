6a98054363edfa9582213482826cf73d9647b4f5
alexxx12345 <alexol123@mail.ru>
2016-08-23 21:38:01 +0300

    Added checking for real pangram and space and uppercase

--- pan.py
+++ pan.py
@@ -7,8 +7,7 @@ def check(text):
 print 	(check('abc')== False)
 print (check('abcdefghijklmnopqrstuvwxyz')== True)
 print (check('ABCDEFGHIJKLMNOPQRSTUVWXYZ') == True)
-
-
+print (check('Quick brown fox jumps over the lazy dog') == True)
 
 
 
